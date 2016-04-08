9ebef58838c649fcc9f68692bb757aeeee294c91
sublio <Sublio@roberts-Mac.local>
2016-04-08 22:33:31 +0600

    Current tasks

--- DMViewController.m
+++ DMViewController.m
@@ -8,7 +8,9 @@
 
 #import "DMViewController.h"
 
-@interface DMViewController ()
+@interface DMViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) UITableView* tableView;
 
 @end
 
@@ -21,6 +23,19 @@
     
     [super loadView];
     
+    
+    CGRect frame = self.view.bounds;
+    frame.origin = CGPointZero;
+    
+    UITableView* tableView = [[UITableView alloc]initWithFrame:frame style:UITableViewStylePlain];
+    tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    
+    [self.view addSubview:tableView];
+    
+    self.tableView = tableView;
 }
 
 - (void)viewDidLoad {
@@ -32,6 +47,54 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return 5;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    static NSString* identifier = @"Cell";
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if(!cell){
+        
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+        
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Section %d, Row %d", indexPath.section,indexPath.row];
+    
+    cell.detailTextLabel.text = @"Value";
+    
+    return cell;
+}
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 5;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    
+    
+    return [NSString stringWithFormat:@"Section  %d header", section];
+}
+
+
 
 
 
