81fe182773f657e18e115387358e35cf9e3cd6d0
sublio <Sublio@roberts-Mac.local>
2016-04-08 21:16:02 +0600

    Initial Commit

---
+++ TableEditingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableEditingTest
+//
+//  Created by sublio on 08/04/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableEditingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableEditingTest
+//
+//  Created by sublio on 08/04/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
